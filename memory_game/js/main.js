1b4a3bb7d721d6500045af04ce250267cd818cc4
Desi Sandor <d.alex.galaxy@gmail.com>
2019-09-17 09:48:25 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,67 @@
-console.log("Up and running!");
+// Array to store all cards
+var cards = [
+{
+	rank:'queen',
+	suit:'hearts',
+	cardImage:'images/queen-of-hearts.png'
+
+},
+{
+	rank:'queen',
+	suit:'diamonds',
+	cardImage:'images/queen-of-diamonds.png'
+
+},
+{
+	rank:'king',
+	suit:'hearts',
+	cardImage:'images/king-of-hearts.png'
+
+},
+{
+	rank:'king',
+	suit:'hearts',
+	cardImage:'images/king-of-hearts.png'
+
+}
+];
+
+// Array to store the cards in play
+var cardsInPlay = [];
+
+// Check for a match
+var checkForMatch = function () {
+  // Check the two cards match - message to user
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+
+  }
+};
+
+//  Function : User flipping a card
+var flipCard = function (cardId) {
+
+  // Console message: which card was flipped .
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
+
+  // Add card to array of cardsInPlay
+  cardsInPlay.push(cards[cardId].rank);
+
+  // Check to see if two cards have been played
+  if (cardsInPlay.length === 2) {
+    // Call the checkForMatch function
+    checkForMatch();
+  }
+};
 
 
-const cards = ["queen", "queen", "king","king"];
-
-
-const  cardsInPlay = [];
-
-
-
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen")
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo)
-console.log("User flipped king")
-
-if (cardsInPlay[0] === cardsInPlay[1]){
-	alert( "You found a match") }
-
-else{alert("Sorry, try again")}
+// For now, we are manually calling the flipCard function
+// to represent a user's play
+// Call the flipCard function, passing in an index as the argument
+flipCard(2);
+// Call the flipCard function, passing in an index as the argument
+flipCard(2);
